b68783a431e4de59c16d922253a431801cc1374d
Adam Kalt <adam.kalt.1@gmail.com>
2018-05-24 19:28:51 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -3,29 +3,22 @@ def selects_all_female_bears_return_name_and_age
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
-  SELECT AVG(net_worth) FROM cats;
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  SELECT AVG(net_worth) FROM cats;
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  SELECT AVG(net_worth) FROM cats;
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  SELECT AVG(net_worth) FROM cats;
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  SELECT AVG(net_worth) FROM cats;
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  SELECT AVG(net_worth) FROM cats;
 end
 
 def selects_bear_that_killed_Tim
-  SELECT AVG(net_worth) FROM cats;
 end
